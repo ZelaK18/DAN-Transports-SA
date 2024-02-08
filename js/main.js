7e668c3db761c7ebdeca53ccf2f8d798bc3b5c78
Castrovinci Dylan <74837533+ZelaK18@users.noreply.github.com>
2024-02-08 16:26:31 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-/*Barre de navigation*/
-const links = document.querySelectorAll("nav li");
-
-icons.addEventListener("click", () => {
-	nav.classList.toggle("active");
-});
-
-links.forEach((link) => {
-	link.addEventListener("click", () => {
-		nav.classList.remove("active");
-	});
-});
-
 /*Counter menu*/
 $(document).ready(function(){
 	$('.num').counterUp({
